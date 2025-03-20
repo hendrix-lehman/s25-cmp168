57db30bee6b5bc6695ed6b685c9be38b8318bf10
Hendrix Tavarez <htavarez@gmail.com>
2025-03-20 19:37:24 -0400

    mar 20

--- file_io/App.java
+++ file_io/App.java
@@ -1,20 +1,29 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 class App {
-  public static void main(String[] args) {
+
+  public static void keyboardInput() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a string: ");
+    String s = sc.nextLine();
+    System.out.println("You entered: " + s);
+    sc.close();
+  }
+
+  public static void fileInput() {
     Scanner sc = null;
     try {
-      File file = new File("test.txt");
-      FileInputStream fis = new FileInputStream(file);
-      sc = new Scanner(fis);
-      while (sc.hasNextLine()) {
-        System.out.println(sc.nextLine());
+      File file = new File("./test.txt");
+      if (file.exists() && file.canRead() && file.isFile()) {
+        FileInputStream fis = new FileInputStream(file);
+        sc = new Scanner(fis);
+        while (sc.hasNextLine()) {
+          System.out.println(sc.nextLine());
+        }
       }
-      // System.out.println("Enter a string: ");
-      // String s = sc.nextLine();
-      // System.out.println("You entered: " + s);
     } catch (Exception e) {
       e.printStackTrace();
       System.out.println("An error occurred.");
@@ -23,5 +32,34 @@ class App {
         sc.close();
       }
     }
+  }
+
+  public static void outputToConsole() {
+    System.out.println("Hello, World!");
+  }
+
+  public static void outputToFile() {
+    try {
+      File file = new File("./output.txt");
+      if (file.exists() && file.canWrite() && file.isFile()) {
+        System.out.println("File exists and is writable.");
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write("Hello, World!".getBytes());
+        fos.close();
+      } else {
+        System.out.println("File does not exist or is not writable.");
+        file.createNewFile();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.out.println("An error occurred.");
+    }
+  }
+
+  public static void main(String[] args) {
+    // keyboardInput();
+    // fileInput();
+    // outputToConsole();
+    outputToFile();
   }
 }
